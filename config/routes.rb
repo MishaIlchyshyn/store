c7b9d545ca8c1613585cd1186c415055524415ad
Misha Ilchyshyn <ilchyshynmuchailo@gmail.com>
2026-05-19 11:20:48 +0300

    Add ability to users sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
+  resource :sign_up
   root "products#index"
   resources :products do
     resources :subscribers, only: %i[create], controller: "products/subscribers"
