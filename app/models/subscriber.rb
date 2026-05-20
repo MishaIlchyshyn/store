9cdeceed3c766799a27cdaab88e5db65af1389f4
Misha Ilchyshyn <ilchyshynmuchailo@gmail.com>
2026-05-20 15:30:14 +0300

    Add subscribers to admins

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -2,4 +2,10 @@ class Subscriber < ApplicationRecord
   generates_token_for :unsubscribe
 
   belongs_to :product
+
+  def self.filter_by(params)
+    results = all
+    results = results.where(product_id: params[:product_id]) if params[:product_id].present?
+    results
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -30,5 +30,6 @@ Rails.application.routes.draw do
     resources :users
     resources :products
     resources :wishlists
+    resources :subscribers
   end
 end
